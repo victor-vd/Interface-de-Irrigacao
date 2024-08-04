f9c16d81233f05970639ad56e5f0ae6f074d0058
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-08-04 05:32:28 -0300

    modificações de estilização

--- Scripts/inject.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("nav").innerHTML = `
-    <a href="PainelPrincipal.html"><img class="imagem" src="../Assets/Imagens/LogoIrrigacao (64x64).png" alt=""></a>
-    <div class="titulo">
-        <span>
-            Sistema de Irrigação
-        </span>
-    </div>
-    <span class="imagem"></span>
-`;
