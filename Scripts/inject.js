4e22a8a3347eb7c3f96bf2c8f00307c56e9d6c92
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-09-27 00:16:48 -0300

    arquivos iniciais adicionados

---
+++ Scripts/inject.js
@@ -0,0 +1,8 @@
+document.querySelector("nav").innerHTML = `
+    <a href="PainelPrincipal.html"><img src="../Assets/Imagens/" alt=""></a>
+    <div class="titulo">
+        <span>
+            Sistema de Irrigação
+        </span>
+    </div>
+`;
